20b244ee1c8b42662d30ca9580acf7cc4e0c8bdc
sonali samantaray <hello.sonali5@gmail.com>
2023-10-09 22:32:43 +0530

    9/10/2023

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Footer from '@/components/common/Footer'
+import TopNavbar from '@/components/common/TopNavbar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <TopNavbar/>
+  <Component {...pageProps} />
+  <Footer/>
+  
+  </>)
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,30 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title>Bizmax - Creative Agency Template</title>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css"/>
+    <link rel="stylesheet" href="assets/css/plugins/fontawesome.min.css"/>
+    <link rel="stylesheet" href="assets/css/plugins/slick.css"/>
+    <link rel="stylesheet" href="assets/css/plugins/animate.css"/>
+    <link rel="stylesheet" href="assets/css/plugins/swiper.min.css"/>
+    <link rel="stylesheet" href="assets/css/plugins/odometer.css"/>
+    <link rel="stylesheet" href="assets/css/default.css"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="assets/js/plugins/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/plugins/isotope.pkg.min.js"></script>
+    <script src="assets/js/plugins/jquery.slick.min.js"></script>
+    <script src="assets/js/plugins/odometer.js"></script>
+    <script src="assets/js/plugins/ripples.min.js"></script>
+    <script src="assets/js/plugins/wow.min.js"></script>
+    <script src="assets/js/plugins/swiper.min.js"></script>
+    <script src="assets/js/main.js"></script>
       </body>
     </Html>
   )
